821b40cda9464dffa9514f49c265ac80c16d63d2
Ivan Kozlov <ivaan.kozlov@gmail.com>
2021-06-20 20:27:05 -0700

    feat: add load_csv method
    add ability to retrieve obj trajectories by filter

--- gstreamer/trajectories.py
+++ gstreamer/trajectories.py
@@ -1,4 +1,5 @@
 import csv
+import copy
 import time
 import hashlib
 from collections import namedtuple, defaultdict
@@ -59,6 +60,54 @@ class ObjTrajectories:
         for box in obj:
           writer.writerow(box._asdict())
 
+  def load_csv(self, filename, debug=False):
+    self.objs_dict = defaultdict(lambda: [])
+    with open(filename, 'r') as csvfile:
+      reader = csv.DictReader(csvfile, fieldnames=DetectedObject._fields)
+      next(reader, None)
+      for row in reader:
+        row['id'] = int(row['id'])
+        row['frame'] = int(row['frame'])
+        row['x'] = float(row['x'])
+        row['y'] = float(row['y'])
+        row['w'] = float(row['w'])
+        row['h'] = float(row['h'])
+        row['cx'] = float(row['cx'])
+        row['score'] = float(row['score'])
+        row['timestamp'] = float(row['timestamp'])
+        detectedObject = DetectedObject(**row)
+        self.objs_dict[detectedObject.id].append(detectedObject)
+    if debug:
+      self.print_debug_info()
+
+  def get_trajectories_svg(self, drawing, start_time=0, end_time=None, track_ids=None, draw_last_rectangle=True):
+    if track_ids is not None:
+      objects = { key: value for key, value in self.objs_dict.items() if key in track_ids}
+    else:
+      objects = copy.deepcopy(self.objs_dict)
+    def filter_by_timestamp(box):
+      after_start_time = box.timestamp >= start_time
+      before_end_time = box.timestamp <= end_time if end_time is not None else True
+      return after_start_time and before_end_time
+    for track_id in objects.keys():
+      boxes_filtered_by_timestamp = list(filter(filter_by_timestamp, objects[track_id]))
+      FRAME_STEP = 5
+      prev_frame_number = None
+      boxes_filtered_by_frame_step = []
+      for box in boxes_filtered_by_timestamp:
+        if prev_frame_number is None or prev_frame_number + FRAME_STEP < box.frame:
+          prev_frame_number = box.frame
+          boxes_filtered_by_frame_step.append(box)
+      objects[track_id] = boxes_filtered_by_frame_step
+    objects = { key: value for key, value in objects.items() if len(value) > 0 }
+    for track_id, points in objects.items():
+      color = 'rgb({},{},{})'.format(*id_to_random_color(track_id))
+      self._update_swg_drawing_from_points(drawing, points, color)
+      if draw_last_rectangle:
+        p = points[-1]
+        drawing.add(drawing.rect(insert=(p.x, p.y), size=(p.w, p.h),
+          fill='none', stroke=color, stroke_width='2'))
+
   def _detect_if_object_has_crossed(self, track_id):
     if self.cross_segment is None:
       return
@@ -94,13 +143,16 @@ class ObjTrajectories:
     for track_id, obj in self.objs_dict.items():
       color = 'rgb({},{},{})'.format(*id_to_random_color(track_id))
       points = self.get_point_with_recall(track_id, frame_recall=5)
-      segments = ObjTrajectories.build_segments_from_points(points)
-      for s in segments:
-        p1, p2 = s
-        if s is segments[0]:
-          drawing.add(drawing.circle(center=p1, r=10, fill=color))
-        drawing.add(drawing.line(start=p1, end=p2, stroke=color, stroke_width='3'))
-        drawing.add(drawing.circle(center=p2, r=10, fill=color))
+      self._update_swg_drawing_from_points(drawing, points, color)
+
+  def _update_swg_drawing_from_points(self, drawing, points, color):
+    segments = ObjTrajectories.build_segments_from_points(points)
+    for s in segments:
+      p1, p2 = s
+      if s is segments[0]:
+        drawing.add(drawing.circle(center=p1, r=3, fill=color))
+      drawing.add(drawing.line(start=p1, end=p2, stroke=color, stroke_width='3'))
+      drawing.add(drawing.circle(center=p2, r=3, fill=color))
     counter_message = 'Clockwise {}. Counterclockwise {}'.format(self._cross_clockwise_counter, self._cross_counter_clockwise_counter)
     drawing.add(drawing.text(counter_message, insert=(10, 10), fill='white', font_size=20))
 
