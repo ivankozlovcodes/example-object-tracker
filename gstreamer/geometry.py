a740a92e06c7ec072b5d136f6945a7dc332a0eb7
Ivan Kozlov <ivaan.kozlov@gmail.com>
2021-06-21 22:30:56 -0700

    fix: cross is detected even if the intersection point does not belong to any segments

--- gstreamer/geometry.py
+++ gstreamer/geometry.py
@@ -4,7 +4,7 @@ def get_line_parameters(p1, p2):
     C = p1[0]*p2[1] - p2[0]*p1[1]
     return A, B, -C
 
-def intersection(line1, line2):
+def get_line_intersection(line1, line2):
     D  = line1[0] * line2[1] - line1[1] * line2[0]
     Dx = line1[2] * line2[1] - line1[1] * line2[2]
     Dy = line1[0] * line2[2] - line1[2] * line2[0]
@@ -15,12 +15,20 @@ def intersection(line1, line2):
     else:
         return None
 
-def segments_intersection(section1, section2):
-  p1, p2 = section1
-  q1, q2 = section2
+def point_belong_to_segment(segment, point):
+  ((x1, y1), (x2, y2)) = segment
+  x3, y3 = point
+  return (min(x1, x2) <= x3 <= max(x1, x2)) and (min(y1, y2) <= y3 <= max(y1, y2))
+
+def segments_intersection(segment1, segment2):
+  p1, p2 = segment1
+  q1, q2 = segment2
   line1 = get_line_parameters(p1, p2)
   line2 = get_line_parameters(q1, q2)
-  return intersection(line1, line2)
+  line_intersection = get_line_intersection(line1, line2)
+  if line_intersection is not None and point_belong_to_segment(segment1, line_intersection) and point_belong_to_segment(segment2, line_intersection):
+    return line_intersection
+  return None
 
 def point_to_segment_orientation(segment, point):
     """Returns True if `point` is clockwise oriented in regards of `segment`"""
